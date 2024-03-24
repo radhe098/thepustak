44dc07da80189257f0524e1252baab004a845c86
Purushottam Tapase <purushottamtapase1@gmail.com>
2024-03-24 09:59:10 +0530

    moved db from local to mongoDB cluster

--- server/index.js
+++ server/index.js
@@ -18,13 +18,27 @@ const uploadRoute = require('./route/uploads');
 app.use('/', userRoute);
 app.use('/', uploadRoute);
 
+// setting mongoose connection
+const uri = "mongodb+srv://radhey:Dholakpur098@cluster0.e3dfdbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
- mongoose.connect('mongodb://127.0.0.1:27017/The_Pustak_').then(() =>{
+ mongoose.connect(uri).then(() =>{
     console.log('Database connected');
  }).catch((e)=>{
     console.log(e)
  })
+//  const db = mongoose.connection;
+//  db.on('connected', () => {
+//    console.log('Connected to MongoDB Atlas');
+//  });
+ 
+//  db.on('error', (err) => {
+//    console.error('Error connecting to MongoDB Atlas:', err);
+//  });
+ 
+//  db.on('disconnected', () => {
+//    console.log('Disconnected from MongoDB Atlas');
+//  });
 
  app.get("/", async (req, res) => {
     res.send("Success !!!!!!");
